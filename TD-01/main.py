c4073bb35b7ba05467cd7334444c39879bd754d2
Yug Merabtène <yug.merabtene@gmail.com>
2025-04-01 10:45:28 +0200

    Update main.py

--- TD-01/main.py
+++ TD-01/main.py
@@ -1,35 +1,35 @@
 ## Scanner de port
 
-#Definir une fonction qui va tester un port specifique
 import threading
-from socket import socket
+import socket
 
-
+# Définir une fonction qui va tester un port spécifique
 def scan_port(host, port):
     try:
-        #creation d'un objet socket
-        sock = socket(socket.AF_INET, socket.SOCK_STREAM)
-        #Definir un delais pour eviter le TIMEOUT et blocage
+        # Création d'un objet socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Définir un délai pour éviter le TIMEOUT et blocage
         sock.settimeout(1)
-        #Tentative de connexion sur le port (0 si la connexion a reussit)
-        result = sock.connect_ex((host,port))
-        #Si le port est ouvert (result == 0), on l'affiche
+        # Tentative de connexion sur le port (0 si la connexion a réussi)
+        result = sock.connect_ex((host, port))
         if result == 0:
-            print (f"[+] Port {port} ouvert")
-        # On ferme le socket
+            print(f"[+] Port {port} ouvert")
         sock.close()
     except Exception as e:
-        #Gestion des erreurs
+        # Gestion des erreurs
         print(f"[-] Erreur sur le port {port}: {e}")
-    # On demande a l'utilisateur l'adresse ip de la cible
-target = input("Entrez l'ip a scanner")
 
-#On demande la plage d'adresse a scanner
-start_port = int(input("Port de debut"))
-end_port = int(input("Port de fin"))
-#on informe l'utilisateur qu'on commence le scan
-print(f"\n[***] scan target {target} sur les ports {start_port} à {end_port} [***]\n")
-for port in range (start_port, end_port +1):
-    #on creer un thread (execution parallele) pour chaque port
+# On demande à l'utilisateur l'adresse IP de la cible
+target = input("Entrez l'IP à scanner: ")
+
+# On demande la plage d'adresses à scanner
+start_port = int(input("Port de début: "))
+end_port = int(input("Port de fin: "))
+
+# On informe l'utilisateur qu'on commence le scan
+print(f"\n[***] Scan de {target} sur les ports {start_port} à {end_port} [***]\n")
+
+# On crée un thread (exécution parallèle) pour chaque port
+for port in range(start_port, end_port + 1):
     t = threading.Thread(target=scan_port, args=(target, port))
     t.start()
